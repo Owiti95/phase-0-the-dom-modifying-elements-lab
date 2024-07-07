445b35c7366fb0b7f566f38710631d27b1e85914
hannington owiti <hanningtonowiti@gmail.com>
2024-07-07 16:20:57 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = "Hannington is the champion";
+document.body.append(newHeader);
+
+
+
